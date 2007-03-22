9a05bc0bbe81650d031a520dbff977da1d920ac4
Ricardo SIGNES <rjbs@cpan.org>
2007-03-22 19:06:24 +0000

    Import of RJBS/Email-Abstract-2.132 from CPAN.
    gitpan-cpan-distribution: Email-Abstract
    gitpan-cpan-version:      2.132
    gitpan-cpan-path:         RJBS/Email-Abstract-2.132.tar.gz
    gitpan-cpan-author:       RJBS
    gitpan-cpan-maturity:     released

--- lib/Email/Abstract.pm
+++ lib/Email/Abstract.pm
@@ -4,7 +4,7 @@ use Email::Simple;
 use 5.006;
 use strict;
 use warnings;
-our $VERSION = '2.131';
+our $VERSION = '2.132';
 use Module::Pluggable search_path => [ __PACKAGE__ ], require => 1;
 
 my @plugins = __PACKAGE__->plugins(); # Requires them.
@@ -217,7 +217,7 @@ a string.
 
 This module is maintained by the Perl Email Project
 
-  L<http://emailproject.perl.org/wiki/Email::Abstract>
+L<http://emailproject.perl.org/wiki/Email::Abstract>
 
 =head1 AUTHOR
 
